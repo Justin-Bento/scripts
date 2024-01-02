f3f56efc2648855f9a8c315129eb57d271e86715
Justin Bento <bento.a.justin@gmail.com>
2024-01-02 17:35:06 -0500

    Included copyright commentts for codde

--- name_modification.rb
+++ name_modification.rb
@@ -1,3 +1,8 @@
+# User: Justin Bento
+# Start Date to End Date: 2023-12-30 to 2024-12-02
+# Purpose:
+# The provided Ruby code is an interactive program that allows a user to input their name and select a casing option from a predefined list. After entering their name, the program prompts the user to choose a casing style, such as lowercase, UPPERCASE, camelCase, snake_case, or kebab-case. The selected casing style is then applied to the user's name, and the modified name is displayed as output.
+
 # Function that changes user input based on the case setting.
 def greeting
   print "Enter some text: "
