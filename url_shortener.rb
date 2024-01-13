c6b91080eb2ba10b88da476cba1add763548e2bb
Justin Bento <bento.a.justin@gmail.com>
2024-01-12 22:43:28 -0500

    Add input validation using URI
    Utilize the URI library to check if the entered input is a valid HTTP or HTTPS URL

--- url_shortener.rb
+++ url_shortener.rb
@@ -1,8 +1,17 @@
+require 'uri'
+
 def place_url
   print "Enter a url: "
   url = gets.chomp
   print "Your revised URL appears as: \n#{url}"
+
+  # Basic input validation using URI
+  if URI.parse(url).is_a?(URI::HTTP) or URI.parse(url).is_a?(URI::HTTPS)
+    puts "Your new URL looks like: #{url}"
+  else
+    puts "Invalid URL entered. Please enter a valid URL."
+  end
 end
 
 
-puts place_url()
+place_url
