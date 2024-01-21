3efd3d3bf76ef4b67d9174de8a0dd59f3c3f0b48
Justin Bento <bento.a.justin@gmail.com>
2024-01-21 12:50:33 -0500

    Add basic mathematical functions
    Implemented subtraction, multiplication, and division functions in Ruby.
    
    These functions take two parameters and perform the specified mathematical operations.

--- formula_forge.rb
+++ formula_forge.rb
@@ -6,5 +6,26 @@ def addition(va1, val2)
   return val1 + val2
 end
 
+def subtraction(val1, val2)
+  return val1 - val2
+end
+
+def multiplication(val1, val2)
+  return val1 * val2
+end
+
+def division(val1, val2)
+  return val1 / val2
+end
+
 result_addition = addition(5, 3)
 puts "Addition: #{result_addition}"
+
+result_subtraction = subtraction(8, 2)
+puts "Subtraction: #{result_subtraction}"
+
+result_multiplication = multiplication(4, 6)
+puts "Multiplication: #{result_multiplication}"
+
+result_division = division(10, 2)
+puts "Division: #{result_division}"
