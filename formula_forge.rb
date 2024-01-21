d3144cdbf29f75032a4cdf6eba0ec10625ca82c6
Justin Bento <bento.a.justin@gmail.com>
2024-01-21 12:41:28 -0500

    Created a new script called formula fordge.

---
+++ formula_forge.rb
@@ -0,0 +1,3 @@
+# Justin Bento
+# Start Date: 2024-01-21 | End Date: 2024-01-
+# Develop a Ruby script within the Formula Forge application to generate a sheet of 24 math problems for Grade 6 students. The problems should include random operators (+, -, *, /) and involve whole numbers and fractions.
