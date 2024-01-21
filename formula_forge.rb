27025c7859892b28a5874167bffb716c1341e64d
Justin Bento <bento.a.justin@gmail.com>
2024-01-21 12:55:58 -0500

    Fixed type mistake for additon from va1 to val1.

--- formula_forge.rb
+++ formula_forge.rb
@@ -2,7 +2,7 @@
 # Start Date: 2024-01-21 | End Date: 2024-01-
 # Develop a Ruby script within the Formula Forge application to generate a sheet of 24 math problems for Grade 6 students. The problems should include random operators (+, -, *, /) and involve whole numbers and fractions.
 
-def addition(va1, val2)
+def addition(val1, val2)
   return val1 + val2
 end
 
