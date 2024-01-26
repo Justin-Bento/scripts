3934b05cb3faecc46af381387fda230b039004e9
Justin Bento <bento.a.justin@gmail.com>
2024-01-25 22:03:06 -0500

    included author, start date, completed date, objective and goal for the script.

--- script_idea_2024_01_25.rb
+++ script_idea_2024_01_25.rb
@@ -1,1 +1,5 @@
-
+# Author: Justin Bento
+# Date Started: 2024-01-25
+# Date Finished: yyyy-mm-dd
+# Objective: The script produces a single output: it generates a random musical key that falls within the specified musical scale.
+# Purpose: It was created to assist me in staying off the internet while practicing guitar, particularly during my improvisation sessions.
