a4f73518c62fdb41ebda20ed4cbf9e17d39dd5ee
Justin Bento <bento.a.justin@gmail.com>
2024-01-25 22:14:12 -0500

    Created a variable that store an array of strings.
    These strings contain the 7 keys of the general music scale. Sharps and flats will be included later after the script is functioning.

--- script_idea_2024_01_25.rb
+++ script_idea_2024_01_25.rb
@@ -3,3 +3,7 @@
 # Date Finished: yyyy-mm-dd
 # Objective: The script produces a single output: it generates a random musical key that falls within the specified musical scale.
 # Purpose: It was created to assist me in staying off the internet while practicing guitar, particularly during my improvisation sessions.
+#
+
+music_scale = ['C', 'D', 'E', 'F,' 'G', 'A', 'B', 'C' ]
+
