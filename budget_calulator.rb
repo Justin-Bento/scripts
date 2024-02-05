6b7cf15ef45e2dd3664d94c6345419ed8ee0806f
Justin Bento <bento.a.justin@gmail.com>
2024-02-04 19:20:14 -0500

    Add 'necessities' method for calculating 50% of after-tax income

--- budget_calulator.rb
+++ budget_calulator.rb
@@ -3,3 +3,7 @@
 # Objective: Calculate my after-tax income using a 50/30/20 budget rule.
 # Purpose: Input my after-tax income and automatically allocate it to my needs and wants without having to go online.
 # While learning rubys input and output, logic opperators work inside ruby,
+
+def necessities(total_value)
+  total_value.to_i * 0.50 # Total value represents 50% of after-tax income
+end
