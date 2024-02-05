a8cf4c0364b4d59ee6e289224d886968dcbf6af7
Justin Bento <bento.a.justin@gmail.com>
2024-02-04 19:15:06 -0500

    Commented the Budget Calculator purpose and objective.

---
+++ budget_calulator.rb
@@ -0,0 +1,5 @@
+# Author: Justin Bento
+# Dates: 2024-02-04 -
+# Objective: Calculate my after-tax income using a 50/30/20 budget rule.
+# Purpose: Input my after-tax income and automatically allocate it to my needs and wants without having to go online.
+# While learning rubys input and output, logic opperators work inside ruby,
