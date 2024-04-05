cc62a0e371171a7b7e1e3df399530f78d7c1f471
Justin Bento <bento.a.justin@gmail.com>
2024-04-05 13:42:50 -0400

    Completed the first 4 tasks

--- sub_strings.rb
+++ sub_strings.rb
@@ -3,9 +3,9 @@
 # End Date:
 # Assignment:
 # -[x] Implement a method #substrings that takes a word as the first argument and have a diconary as a second argument.
-# -[]  The first argument should be an array of valid substrings which is your dictionary.
-# -[]  The 2nd argument returns a hash listing each substring that was found in the original string.
-# -[]  Figure out how many times each substring was found the original string .
+# -[x]  The first argument should be an array of valid substrings which is your dictionary.
+# -[x]  The 2nd argument returns a hash listing each substring that was found in the original string.
+# -[x]  Figure out how many times each substring was found the original string .
 # -[]  Lastly, make sure your method can handle multiple words:.
 # -[]  Please note the order of your keys might be different.
 
