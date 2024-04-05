e52760b7cbc86d3d8aa1528195723cf4793bf149
Justin Bento <bento.a.justin@gmail.com>
2024-04-05 11:08:45 -0400

    transfered external ruby code to ruby scripts folder

---
+++ sub_strings.rb
@@ -0,0 +1,16 @@
+# Author: Justin Bento
+# Start Date: 2024-03-18
+# End Date:
+# Assignment:
+# -[x] Implement a method #substrings that takes a word as the first argument and have a diconary as a second argument.
+# -[]  The first argument should be an array of valid substrings which is your dictionary.
+# -[]  The 2nd argument returns a hash listing each substring that was found in the original string.
+# -[]  Figure out how many times each substring was found the original string .
+# -[]  Lastly, make sure your method can handle multiple words:.
+# -[]  Please note the order of your keys might be different.
+
+def substrings(word, diconary)
+  "Hello my name is #{word} and I am a #{diconary}"
+end
+
+puts substrings("Justin", "Human")
