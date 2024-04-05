a37719a4350235366abf790d81b29650187614e1
Justin Bento <bento.a.justin@gmail.com>
2024-04-05 19:28:40 -0400

    broke down the steps into a logical format

--- stock_picker.rb
+++ stock_picker.rb
@@ -4,5 +4,17 @@
 # End Date:
 # Assignment Breakdown:
 # ------------------------------------------------------------------------------
+# [] Implement a method called "stock_picker."
+# [] Create a variable that stores an array of stock prices.
+# [] The method should return two values:
+#     - The first pair for the best day to buy.
+#     - The second pair for the best day to sell.
+#     Keep in mind that the day starts with zero.
+# [] Quick Tip:
+#     You need to buy before you can sell.
+#     Pay attention to edge cases like:
+#     - The lowest day being the last day.
+#     - The highest day being the first day.
+#
 # Pseudo Code:
 # ------------------------------------------------------------------------------
