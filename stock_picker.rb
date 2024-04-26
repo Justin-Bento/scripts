4282684b1b48dfb6f580a32756a2e3d42ee7b3f4
Justin Bento <bento.a.justin@gmail.com>
2024-04-26 10:02:12 -0400

    updated spacing within the document

--- stock_picker.rb
+++ stock_picker.rb
@@ -29,14 +29,17 @@
 def stock_picker(prices)
   max_profit = 0
   best_days = []
-  prices[buy_day+1..].each_with_index do |sell_price, sell_day|
-    profit = sell_price - buy_price
+
+  prices.each_with_index do |buy_price, buy_day|
+    prices[buy_day+1..].each_with_index do |sell_price, sell_day|
+      profit = sell_price - buy_price
       if profit > max_profit
         max_profit = profit
         best_days = [buy_day, sell_day + buy_day + 1]
       end
     end
   end
+
   best_days
 end
 
