69568c5acff8c99cb24f25c82f1f9f0188a46f82
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2024-07-17 21:22:23 -0400

    Create an instance of MusicScale for F Major, and G Major Scale

--- music_key_part_II.rb
+++ music_key_part_II.rb
@@ -9,5 +9,8 @@ end
 C_Major = MusicScale.new("C Major", ["C", "D", "E", "F", "G", "A", "B", "C"])
 D_Major = MusicScale.new("D Major", ["D", "E", "F#", "G", "A", "B", "C#", "D"])
 E_Major = MusicScale.new("E Major", ["E", "F#", "G#", "A", "B", "C#", "D#", "E"])
+F_Major = MusicScale.new("F Major", ["F", "G", "A", "Bb", "C", "D", "E", "F"])
+G_Major = MusicScale.new("G Major", ["G", "A", "B", "C", "D", "E", "F", "G"])
+
 
 puts C_Major.scale.sample
