f1012d7abfec344fc0843b88a12d60c080ea4793
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2024-07-17 21:20:20 -0400

    Create an instance of MusicScale for E Major scale

--- music_key_part_II.rb
+++ music_key_part_II.rb
@@ -8,5 +8,6 @@ end
 
 C_Major = MusicScale.new("C Major", ["C", "D", "E", "F", "G", "A", "B", "C"])
 D_Major = MusicScale.new("D Major", ["D", "E", "F#", "G", "A", "B", "C#", "D"])
+E_Major = MusicScale.new("E Major", ["E", "F#", "G#", "A", "B", "C#", "D#", "E"])
 
 puts C_Major.scale.sample
