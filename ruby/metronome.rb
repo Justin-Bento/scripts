cc5aca003fc930f8ed1a10a0d63794c75da51b16
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2025-06-11 22:26:24 -0400

    Updated the document metadata to cover 5'ws of product design.

--- ruby/metronome.rb
+++ ruby/metronome.rb
@@ -1,7 +1,9 @@
 # Date Created: Wednesday 11th June 2025
 # Document Version: 0.1.0
 # Author: Justin Bento
+# Platform: Command-line Ruby application
+# Repository: https://github.com/justinbento/scripts/ruby/metronome-timer
 # Description: This project's goal is to help people improve their timing using a metronome.
 #              The metronome will include features such as going silent for a few measures and reappearing at random intervals,
 #              allowing users to test their internal sense of rhythm.
-#              The purpose is to automate this process so the user can keep playing without needing to manually pause or resume the track.
+# Objective: To automate silent intervals so users can stay in flow without stopping the metronome.
