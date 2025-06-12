fa87e982e05777085825e38f43a2ad806376a1f0
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2025-06-11 22:19:10 -0400

    Initialize project with file header with version, date, and purpose.

---
+++ ruby/metronome.rb
@@ -0,0 +1,7 @@
+# Date Created: Wednesday 11th June 2025
+# Document Version: 0.1.0
+# Author: Justin Bento
+# Description: This project's goal is to help people improve their timing using a metronome.
+#              The metronome will include features such as going silent for a few measures and reappearing at random intervals,
+#              allowing users to test their internal sense of rhythm.
+#              The purpose is to automate this process so the user can keep playing without needing to manually pause or resume the track.
